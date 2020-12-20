3dbbb1de19dbd1f052ee57c15cc5f55723b00f9a
2e12 <gabriel.beutler@4teamwork.ch>
2020-12-20 16:53:34 +0100

    Create new homepage

--- script.js
+++ script.js
@@ -1,40 +1,6 @@
-  document.addEventListener("DOMContentLoaded", function(event) {
+ document.addEventListener("DOMContentLoaded", function(event) {
     fadePageIn();
-    loadCards();
-  });
-function loadCards(){
-    let cards = document.getElementsByClassName("card");
-    let i;
-    for (i = 0; i < cards.length; i++) {
-      let card = cards[i];
-      card.style.opacity = 0;
-      let dataUrl = card.getAttribute("data-json");
-      if(dataUrl !== null){
-
-        fetch(dataUrl).then(function(response) {
-          return response.json();
-        })
-
-        .then(function(json) {
-          card.onclick = function(){
-            showReference(json);
-          }
-
-          let imageUrl = json.image;
-          let image = new Image();
-          image.src = imageUrl;
-          image.onload = function(){
-            card.style.background = "url(" + imageUrl + ")";
-            fadeInObject(card);
-          }
-
-          card.getElementsByTagName("h3")[0].innerHTML = json.name;
-
-        });
-
-      }
-    } 
-}
+});
 
 function fadePageIn(){
   let elements = document.getElementsByClassName("fadein");
@@ -45,7 +11,7 @@ function fadePageIn(){
     setTimeout(function() { 
       fadeInObject(element);
     }, delay);
-    delay += 200;
+    delay += 350;
   }
 }
 
@@ -57,42 +23,4 @@ function fadeInObject(element){
 function fadeOutObject(element){
   element.style.transform = "translateY(0)";
   element.style.opacity = 0;
-}
-
-function showReference(data){
-  
-
-  document.getElementById("ref_header").style.background = "url(" + data.image + ")";
-  document.getElementById("ref_title").innerHTML = data.name;
-  document.getElementById("ref_slogan").innerHTML = data.slogan;
-  document.getElementById("ref_description").innerHTML = data.description;
-  if(data.preview !== undefined){
-    document.getElementById("ref_preview").style.display = "inline-block";
-    document.getElementById("ref_preview").href = data.preview;
-  }else{
-    document.getElementById("ref_preview").style.display = "none";
-  }
-  if(data.video !== undefined){
-    document.getElementById("ref_video").style.display = "inline-block";
-    document.getElementById("ref_video").href = data.video;
-  }else{
-    document.getElementById("ref_video").style.display = "none";
-  }
-
-  if(data.git !== undefined){
-    document.getElementById("ref_git").style.display = "inline-block";
-    document.getElementById("ref_git").href = data.git;
-  }else{
-    document.getElementById("ref_git").style.display = "none";
-  }
-
-  document.getElementById("ref_back").onclick = function(){
-    document.getElementById("detail_view").style.display = "none";
-    document.getElementById("main").style.display = "grid";
-    document.documentElement.scrollTop = 0;
-  };
-  document.getElementById("main").style.display = "none";
-  document.getElementById("detail_view").style.display = "grid";
-
-
 }
